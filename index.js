aa863377611c52f30c9beb3e2f7a4341b0ebd85d
Jerome Miranda <jerome.m.miranda@gmail.com>
2019-06-24 15:01:52 +0800

    v0.0.3

--- index.js
+++ index.js
@@ -1,18 +1,12 @@
 import React from 'react';
 
 export default props => {
-	const { headerStyle = {}, id, style = {}, ...defaultProps } = props;
+	const { id, style = {}, ...defaultProps } = props;
 
 	return {
 		...defaultProps,
 		filterable: false,
 		style: { ...style, display: 'flex', alignItems: 'center', justifyContent: 'center' },
-		headerStyle: {
-			...headerStyle,
-			overflow: editable ? 'visible' : 'hidden',
-			display: 'flex',
-			alignItems: 'center'
-		},
 		Cell: ({ value }) => {
 			if (value != '' && value != 'undefined' && typeof value != 'undefined') {
 				value = JSON.parse(value);
